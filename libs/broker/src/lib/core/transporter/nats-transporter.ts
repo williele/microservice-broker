2c3f2b4f155e40c43940491b8cf054c08d6ae57f
Willie Le <williele99@gmail.com>
2021-03-19 05:01:11 +0700

    refactor(broker): :zap: use arvo binary for encode nats transporter

--- libs/broker/src/lib/core/transporter/nats-transporter.ts
+++ libs/broker/src/lib/core/transporter/nats-transporter.ts
@@ -1,11 +1,11 @@
 import { BaseTransporter } from './transporter';
 import { TransportPacket } from '../interface';
 import type { ClientOpts, Client } from 'nats';
-import type { MessagePack } from 'msgpack5';
+import type { Type } from 'avsc';
 import { packageLoader } from '../utils/package-loader';
 
 let nats: typeof import('nats') = undefined;
-let msgpack5: typeof import('msgpack5') = undefined;
+let avsc: typeof import('avsc') = undefined;
 
 /**
  * Nats transporter dependencies
@@ -16,17 +16,23 @@ export class NatsTransporter extends BaseTransporter {
   transporterName = 'nats';
 
   private client: Client;
-  private msgpack: MessagePack;
+  // private msgpack: MessagePack;
+  private packetType: Type;
 
   constructor(private clientOps: ClientOpts) {
     super();
 
     nats = packageLoader('nats', 'NatsTransporter', () => require('nats'));
-    msgpack5 = packageLoader('msgpack5', 'NatsTransporter', () =>
-      require('msgpack5')
-    );
+    avsc = packageLoader('avsc', 'NatsTransporter', () => require('avsc'));
 
-    this.msgpack = msgpack5();
+    this.packetType = avsc.Type.forSchema({
+      name: 'packet',
+      type: 'record',
+      fields: [
+        { name: 'header', type: { type: 'map', values: 'string' } },
+        { name: 'body', type: 'bytes' },
+      ],
+    });
   }
 
   async connect() {
@@ -80,7 +86,7 @@ export class NatsTransporter extends BaseTransporter {
     }
 
     this.client.subscribe(subject, (message, reply) => {
-      const unpack = this.msgpack.decode(message);
+      const unpack = this.packetType.fromBuffer(message);
       if (reply) unpack.header['reply'] = reply;
 
       callback(unpack, reply);
@@ -97,13 +103,13 @@ export class NatsTransporter extends BaseTransporter {
     }
 
     // Encode into messagepack before send out
-    const pack = this.msgpack.encode(packet);
+    const pack = this.packetType.toBuffer(packet);
     return new Promise((resolve, reject) => {
       this.client.requestOne(subject, pack, 5_000, (response) => {
         if (response instanceof nats.NatsError) {
           reject(response);
         } else {
-          const unpack = this.msgpack.decode(response);
+          const unpack = this.packetType.fromBuffer(response);
           resolve(unpack);
         }
       });
@@ -112,7 +118,7 @@ export class NatsTransporter extends BaseTransporter {
 
   async send(subject: string, packet: TransportPacket) {
     return new Promise<void>((resolve) => {
-      const pack = this.msgpack.encode(packet);
+      const pack = this.packetType.toBuffer(packet);
       this.client.publish(subject, pack, () => resolve());
     });
   }
