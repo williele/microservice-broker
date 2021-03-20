4f552c582c66138853ce6abfc8fd1d6b3cd9cfcc
Willie Le <williele99@gmail.com>
2021-03-21 03:30:08 +0700

    feat(nest): :sparkles: add module async config

--- apps/nest-demo/src/app/app.module.ts
+++ apps/nest-demo/src/app/app.module.ts
@@ -1,30 +1,40 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BrokerModule } from '@williele/broker-nest';
 import { initTracer } from 'jaeger-client';
+import { Tracer } from 'opentracing';
 
 import { DemoModule } from './demo/demo.module';
 
-const logger = new Logger('Tracer');
-
 @Module({
-  imports: [
-    BrokerModule.forRoot({
-      serviceName: 'test',
-      serializer: { name: 'arvo' },
-      transporter: { name: 'nats', options: {} },
-      tracer: initTracer(
+  providers: [
+    {
+      provide: Tracer,
+      useValue: initTracer(
         {
           serviceName: 'test-service',
           sampler: { type: 'const', param: 1 },
           reporter: { logSpans: false },
         },
-        {
-          logger: {
-            info: (msg) => logger.log(msg),
-            error: (msg) => logger.error(msg),
-          },
-        }
+        {}
       ),
+    },
+  ],
+  exports: [Tracer],
+})
+class TracerModule {}
+
+@Module({
+  imports: [
+    TracerModule,
+    BrokerModule.forRootAsync({
+      import: [TracerModule],
+      factory: (tracer: Tracer) => ({
+        serviceName: 'test',
+        serializer: { name: 'arvo' },
+        transporter: { name: 'nats', options: {} },
+        tracer,
+      }),
+      inject: [Tracer],
     }),
     DemoModule,
   ],
--- libs/nest/src/lib/broker.module.ts
+++ libs/nest/src/lib/broker.module.ts
@@ -1,23 +1,61 @@
-import { Module, DynamicModule } from '@nestjs/common';
+import {
+  Module,
+  DynamicModule,
+  Provider,
+  ModuleMetadata,
+} from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
+import { InjectorDependency } from '@nestjs/core/injector/injector';
 import { Broker, BrokerConfig } from '@williele/broker';
 import { BrokerBuilderService } from './broker-builder.service';
 import { BrokerServer } from './broker-server';
 import { BROKER_TOKEN } from './constant';
 
+const BROKER_CONFIG_TOKEN = 'BROKER_CONFIG_TOKEN';
+
 @Module({
   imports: [DiscoveryModule],
-  providers: [BrokerBuilderService],
+  providers: [BrokerBuilderService, BrokerServer],
+  exports: [BrokerServer],
 })
 export class BrokerModule {
   static forRoot(config: BrokerConfig): DynamicModule {
     const broker = new Broker(config);
+    const brokerProvider: Provider = {
+      provide: BROKER_TOKEN,
+      useValue: broker,
+    };
 
     return {
-      global: true,
       module: BrokerModule,
-      providers: [{ provide: BROKER_TOKEN, useValue: broker }, BrokerServer],
-      exports: [BrokerServer],
+      providers: [brokerProvider],
+      exports: [BROKER_TOKEN],
+    };
+  }
+
+  static forRootAsync(options: {
+    factory: (...args) => BrokerConfig;
+    inject: InjectorDependency[];
+    import?: ModuleMetadata['imports'];
+  }): DynamicModule {
+    const brokerProvider: Provider = {
+      provide: BROKER_TOKEN,
+      useFactory: (config: BrokerConfig) => new Broker(config),
+      inject: [BROKER_CONFIG_TOKEN],
+    };
+
+    return {
+      module: BrokerModule,
+      imports: options.import || [],
+      providers: [
+        {
+          provide: BROKER_CONFIG_TOKEN,
+          useFactory: (...args) => options.factory(...args),
+          inject: options.inject,
+        },
+        brokerProvider,
+      ],
+      exports: [BROKER_TOKEN],
     };
   }
 }
