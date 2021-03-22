6a50bea6416ee2c42f0256ca082fd74df3dccfcf
Willie Le <williele99@gmail.com>
2021-03-23 00:30:48 +0700

    fix(nest): :bug: fix scan empty method

--- libs/nest/src/lib/broker-builder.service.ts
+++ libs/nest/src/lib/broker-builder.service.ts
@@ -81,7 +81,7 @@ export class BrokerBuilderService implements OnModuleInit {
     const methods = [];
     for (const { name, handler, provider } of methodConfigs) {
       const method = await this.configMethod(name, handler, provider);
-      methods.push(method);
+      if (method) methods.push(method);
     }
 
     return methods;
